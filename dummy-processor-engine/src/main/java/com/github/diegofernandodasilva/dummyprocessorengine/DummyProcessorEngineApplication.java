d3179b87ff086a98b13098ab8a7e03d5bedd5397
DiegoFernandodaSilva <35530088+DiegoFernandodaSilva@users.noreply.github.com>
2020-12-30 22:30:28 +0100

    Dummy processor consume covid19tracker events (#6)
    * dummy-processor_consume_covid19trackerEvents
    
    * dummy-processor_consume_covid19trackerEvents
    
    * Revert "dummy-processor_consume_covid19trackerEvents"
    
    This reverts commit ccf60b10

--- dummy-processor-engine/src/main/java/com/github/diegofernandodasilva/dummyprocessorengine/DummyProcessorEngineApplication.java
+++ dummy-processor-engine/src/main/java/com/github/diegofernandodasilva/dummyprocessorengine/DummyProcessorEngineApplication.java
@@ -1,36 +1,13 @@
 package com.github.diegofernandodasilva.dummyprocessorengine;
 
-import com.github.diegofernandodasilva.dummyprocessorengine.kafka.producer.SampleProducer;
-import com.github.diegofernandodasilva.microservices.playground.Sample;
-import com.github.diegofernandodasilva.microservices.playground.typeMsg;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DummyProcessorEngineApplication implements CommandLineRunner {
-
-    @Autowired
-    private SampleProducer sampleProducer;
+public class DummyProcessorEngineApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(DummyProcessorEngineApplication.class, args);
     }
 
-
-    @Override
-    public void run(String... args) throws Exception {
-        for (int i = 0; i < 10; i++) {
-            sampleProducer.produceSample(sample(i));
-        }
-    }
-
-    private Sample sample(int id) {
-        return Sample.newBuilder()
-                .setId(String.valueOf(id))
-                .setMessage("Message number " + id)
-                .setType(id % 2 == 0 ? typeMsg.TYPE_TWO : typeMsg.TYPE_ONE)
-                .build();
-    }
 }
--- dummy-processor-engine/src/main/java/com/github/diegofernandodasilva/dummyprocessorengine/kafka/consumer/CountryCovid19StatisticsConsumer.java
+++ dummy-processor-engine/src/main/java/com/github/diegofernandodasilva/dummyprocessorengine/kafka/consumer/CountryCovid19StatisticsConsumer.java
@@ -1,4 +1,4 @@
-package com.github.diegofernandodasilva.dummyprocessorengine.kafka.listner;
+package com.github.diegofernandodasilva.dummyprocessorengine.kafka.consumer;
 
 import com.github.diegofernandodasilva.microservices.playground.Sample;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class SampleConsumer {
+public class CountryCovid19StatisticsConsumer {
 
-    @KafkaListener(topics = "${sample.topic}", groupId = "${spring.application.name}")
+    @KafkaListener(topics = "${microservices.kafka.topic.country.covid19.statistics.changed}", groupId = "${spring.application.name}")
     public void listen(@Payload ConsumerRecord<Void, Sample> record) throws InterruptedException {
       log.info("Kafka listener receive message, value={}", record.value());
       Thread.sleep(10_000);
